4a9edfd72be4fa8626ea497b6b67010cb90cc232
rawwchit <rachitsai2@gmail.com>
2026-07-06 01:21:01 +0530

    feat: add user db model

---
+++ backend/app/models/user.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+from app.db.database import Base
+from sqlalchemy.orm import func
+from sqlalchemy.orm import Mapped, mapped_column
+class User(Base):
+    __tablename__ = "users"
+    id:Mapped[int]=mapped_column(
+        primary_key=True
+        )
+    username:Mapped[str] = mapped_column(
+        unique=True
+        )
+    email:Mapped[str] = mapped_column(
+        unique=True
+    )
+    hashed_password:Mapped[str] = mapped_column(
+        
+    )
+    is_active:Mapped[bool] = mapped_column(
+        default = True
+    )
+    created_at:Mapped[datetime] = mapped_column(
+        server_default=func.now()
+    )
+    updated_at:Mapped[datetime] = mapped_column(
+        server_default=func.now()
+        onupdate=func.now()
+    )
